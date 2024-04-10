9edf7e4a5ecdcd497411f43032a7dbe4a539a07b
11200 <1120095299@qq.com>
2024-04-10 16:57:08 +0800

    加一个直接调用的方法

--- webserver/src/service/theardpool.py
+++ webserver/src/service/theardpool.py
@@ -6,3 +6,7 @@ from service.train import do_train
 def thread_runner(thread_num, func, *args):
     executor = ThreadPoolExecutor(thread_num)
     f = executor.submit(do_train, *args)
+
+
+def direct_call_do_train(table_name, database_path):
+    do_train(table_name, database_path)
